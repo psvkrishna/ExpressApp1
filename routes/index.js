acf73e30c7f87d00eadcbc612e5a057c361c6add
psvkrishna <psv_krishna@outlook.com>
2015-11-22 16:42:37 +0530

    First Iteration

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+﻿
+/*
+ * GET home page.
+ */
+
+exports.index = function (req, res) {
+    res.render('index', { title: 'Express', year: new Date().getFullYear() });
+};
+
+exports.about = function (req, res) {
+    res.render('about', { title: 'About', year: new Date().getFullYear(), message: 'Your application description page' });
+};
+
+exports.contact = function (req, res) {
+    res.render('contact', { title: 'Contact', year: new Date().getFullYear(), message: 'Your contact page' });
+};
+
+exports.test = function (req, res){
+    res.render('Test', { title: 'Test Page', year: new Date().getFullYear(), message: 'My Test page' });
+}
